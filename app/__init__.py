814ebd4fc1e316e6da173adad65b4b3c8fa837aa
Angela Kormushoska <angela.kormushoska@scalefocus.com>
2025-07-29 16:36:57 +0200

    Add dependency container and init

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,39 @@
 from flask import Flask
-from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
+from flask_restx import Api
 
-from .configuration.config import Config
+from app.configuration.config import Config
+from app.container import Container
+# from app.routes.user_route import user_ns
+from app.extensions import db
+from app.services import user_service
 
+# Function to set up REST API and Swagger API
+def create_api(app: Flask):
+    api = Api(
+        app,
+        title="Event Finder API",
+        version="1.0",
+        description="REST API",
+        doc="/swagger/"  # optional: where Swagger UI lives
+    )
+    #api.add_namespace(user_ns, path="/users")
+
+
+# Main app factory function for Flask to create the app instance
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    # Initialize extensions
+    db.init_app(app)
+
+    # Dependency injection
+    container = Container()
+    container.init_resources()
+    container.wire(modules=[
+        "app.routes.user_route"
+    ])
+
+    create_api(app)
 
     return app
---
+++ app/container.py
@@ -0,0 +1,18 @@
+from dependency_injector import containers, providers
+from app.extensions import db
+# from app.services.user_service import UserService
+
+
+# Define a container class using DeclarativeContainer base class
+class Container(containers.DeclarativeContainer):
+    wiring_config = containers.WiringConfiguration(packages=["app.routes", "app.services"])
+
+    db_session = providers.Singleton(lambda: db.session)
+
+    # Repositories
+    # Services
+
+    # Define a factory for the UserService
+    user_service = providers.Factory(
+        # UserService
+    )
