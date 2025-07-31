c4b71ff03f7932b53cd437d0f439065b1ebc6b1e
Angela Kormushoska <angela.kormushoska@scalefocus.com>
2025-07-31 15:25:57 +0200

    Add event model and update user model

--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,7 @@
 from app.extensions import db
+from app.models.event import Event
 from app.util.user_util import NAME_MAX_LENGTH,SURNAME_MAX_LENGTH,EMAIL_MAX_LENGTH,PASSWORD_MAX_LENGTH
+from app.models.event import guest_list
 
 class User(db.Model):
     """
@@ -17,6 +19,20 @@ class User(db.Model):
     surname=db.Column(db.String(SURNAME_MAX_LENGTH), nullable=False)
     email = db.Column(db.String(EMAIL_MAX_LENGTH), nullable=False, unique=True)
     password=db.Column(db.String(PASSWORD_MAX_LENGTH), nullable=False)
+
+    # MANY TO MANY
+    events_attending = db.relationship(
+        'Event',
+        secondary=guest_list,
+        back_populates='guests',
+        lazy='dynamic'
+    )
+    # ONE TO MANY
+    organized_events = db.relationship(
+        'Event',
+        back_populates='organizer',
+        lazy='dynamic'
+    )
 
     def __repr__(self):
         """
